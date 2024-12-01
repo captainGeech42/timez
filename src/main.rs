055dd1cb095d88b22e91babfd65a325bfce82999
Zander Work <zander@zanderwork.com>
2024-12-01 18:17:36 -0500

    can do basic time conversion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
+use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Utc};
+use chrono_tz::{OffsetName, Tz};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Show the current time in the configured timezones
+    Now,
+}
+
+trait TimestampString {
+    /// Convert the UTC timestamp to a consistent string representation.
+    fn format_ts(&self) -> String;
+}
+
+/// Format string for TZ-aware chrono timestamp objects.
+// specifiers: https://docs.rs/chrono/0.4.38/chrono/format/strftime/index.html
+const TZ_FSTR: &str = "%Y-%m-%d %H:%M:%S %Z";
+
+/// Format string for TZ-naive chrono timestamp objects.
+const NTZ_FSTR: &str = "%Y-%m-%d %H:%M:%S";
+
+/// List of timezones to show.
+const TIMEZONES: &[&str] = &["Europe/London", "America/Chicago", "Asia/Kabul"];
+
+impl TimestampString for DateTime<Utc> {
+    fn format_ts(&self) -> String {
+        return self.format(TZ_FSTR).to_string();
+    }
+}
+
+impl TimestampString for DateTime<Local> {
+    fn format_ts(&self) -> String {
+        return self.format(TZ_FSTR).to_string();
+    }
+}
+
+impl TimestampString for NaiveDateTime {
+    fn format_ts(&self) -> String {
+        return self.format(NTZ_FSTR).to_string();
+    }
+}
+
+impl TimestampString for DateTime<Tz> {
+    fn format_ts(&self) -> String {
+        let offset = self
+            .timezone()
+            .offset_from_local_datetime(&self.naive_local())
+            .unwrap();
+
+        // Not all timezones have an available abbreviation, so fallback to the hours delta (eg, `+0430`)
+        // if an abbreviation isn't available.
+        // Example: `Asia/Kabul`
+        let abbr = match offset.abbreviation() {
+            Some(x) => x.to_string(),
+            None => offset.to_string(),
+        };
+
+        return format!("{} {}", self.format(NTZ_FSTR).to_string(), abbr);
+    }
+}
+
+fn cmd_now() {
+    let ts = Local::now();
+
+    println!("Current time is: {}", ts.format_ts());
+
+    for tz_str in TIMEZONES.iter() {
+        let tz: Tz = tz_str.parse().unwrap();
+        let converted_ts = ts.with_timezone(&tz);
+
+        println!("Time in {}: {}", tz_str, converted_ts.format_ts(),);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Now) => {
+            cmd_now();
+        }
+        None => {}
+    }
 }

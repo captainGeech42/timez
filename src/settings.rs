70660780f5617769208ed111a6d5d631fe1069d2
Zander Work <zander@zanderwork.com>
2024-12-01 21:04:23 -0500

    add config loading from homedir

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,6 @@
 use chrono_tz::Tz;
 use config::{Config, ConfigError, File};
+use homedir::my_home;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -11,7 +12,13 @@ pub(crate) struct Settings {
 impl Settings {
     pub(crate) fn new() -> Result<Settings, ConfigError> {
         let s = Config::builder()
-            .add_source(File::with_name("timez"))
+            .add_source(File::with_name("timez").required(false))
+            .add_source(
+                File::with_name(
+                    format!("{}/.timez", my_home().unwrap().unwrap().to_str().unwrap()).as_str(),
+                )
+                .required(false),
+            )
             .build()?;
 
         s.try_deserialize()
